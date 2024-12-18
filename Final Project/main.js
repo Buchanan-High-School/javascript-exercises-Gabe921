1c13aeb02230d0fca042bcbd49d89d04d7b5320f
gabe <gxkappes@gmail.com>
2024-12-18 11:47:16 -0500

    Finished the project by adding a price counter

--- Final Project/main.js
+++ Final Project/main.js
@@ -8,14 +8,7 @@ console.log(filteredProducts)
 const productsContainer = document.querySelector(".notsidebar")
 const searchForm = document.querySelector(".input-form")
 const searchInput = document.querySelector(".search-input")
-const priceButton = document.querySelector(".PriceAdd")
-
-priceButton.addEventListener("click",() =>{
-    console.log("Clicked!")
-})
-
-
-
+const cntDisplay = document.querySelector("#counterDisplay")
 
 // Create a function for the search bar
 searchForm.addEventListener("keyup",() => {
@@ -42,7 +35,7 @@ function displayProducts() {
                     <footer>
                         <p class="product-name">${product.title}</p>
                         <p class="product-price">${"$"+product.price}</p>
-                        <button class="product-purchase">${product.button}
+                        <button data-cost=${product.price}  class="product-purchase">${product.button}</button>
                     </footer>
                 </article>`
     // Below is how to remove commas
@@ -51,3 +44,30 @@ function displayProducts() {
 
 // Run the function
 displayProducts()
+
+
+
+// Cart Display Function
+
+const purchaseButton = document.querySelector(".Purchase")
+const priceButtons = document.querySelectorAll(".product-purchase")
+
+let currentPrice = 0
+priceButtons.forEach(item => {
+    console.log(item)
+    item.addEventListener("click", calculatePrice)
+})
+
+function calculatePrice(event) {
+    console.log(event.target.dataset.cost)
+    // The "+="" adds the current value to the added value
+    // The Number property forces the target to be read as a number
+    currentPrice+= Number(event.target.dataset.cost) 
+    // Add the "$" to the inner.Text, NOT the number
+    cntDisplay.innerText= "$" + currentPrice
+}
+// This purchase button doesn't refresh the page
+purchaseButton.addEventListener("click", () => {
+    console.log("Clicked!")
+    cntDisplay.innerText = "Purchased! Now refresh to continue shopping!"
+})
